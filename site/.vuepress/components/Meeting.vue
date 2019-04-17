4c4edd0975c9cf1f5ce9fd7e73639df871025fa8
henry.wang <hi@henry.wang>
2019-04-17 15:51:11 +0100

    feat: add progress of todo

--- site/.vuepress/components/Meeting.vue
+++ site/.vuepress/components/Meeting.vue
@@ -1,36 +1,54 @@
 
 <template>
-<div class="meetingBlock" :id="date">
-  <table>
-    <tr>
-      <th>No</th>
-      <th>Date</th>
-      <th>Next Meeting</th>
-      <th>Members</th>
-    </tr>
-    <tr>
-      <td>{{index}}</td>
-      <td>{{date}}</td>
-      <td>{{nextDate}}</td>
-      <td>{{members}}</td>
-    </tr>
-  </table>
-  <p>
-    <h4>Topic Discussed:</h4>
-    <slot name="topic">
-    </slot>
-  </p>
-  <p>
-    <h4>Progress:</h4>
-    <slot name="prog">
-    </slot>
-  </p>
-  <p>
-    <h4>Todo:</h4>
-    <slot name="todo">
-    </slot>
-  </p>
-</div>
+  <div class="meetingBlock" :id="date">
+    <table>
+      <tr>
+        <th>No</th>
+        <th>Date</th>
+        <th>Next Meeting</th>
+        <th>Members</th>
+      </tr>
+      <tr>
+        <td>{{index}}</td>
+        <td>{{date}}</td>
+        <td>{{nextDate}}</td>
+        <td>{{members}}</td>
+      </tr>
+    </table>
+    <p>
+      <h4>Topic Discussed:</h4>
+      <slot name="topic">
+      </slot>
+    </p>
+    <p>
+      <h4>Progress:</h4>
+      <slot name="prog">
+      </slot>
+    </p>
+    <p>
+      <h4>Todo:</h4>
+      <table>
+        <tr>
+          <th>
+            Todo
+          </th>
+          <th>
+            Progress
+          </th>
+        </tr>
+        <tr>
+          <td valign="top">
+            <slot name="todo">
+            </slot>
+          </td>
+          <td valign="top">
+            <slot name="todoProg">
+            </slot>
+          </td>
+        </tr>
+      </table>
+    </p>
+  </div>
 </template>
 <script>
 export default {
@@ -60,5 +78,5 @@ li.params p {
   }
 .meetingBlock {
   margin: 1rem 0 2rem;
-}
+  }
 </style>
